7e077ef0ca345185e22a6f114f550848f1e9358a
Islarky Gwapito <islarky88@gmail.com>
2022-09-01 11:22:22 +0800

    and design vue install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
+  css: ['assets/css/tailwind.css'],
+  plugins: ['@/plugins/ant-design-vue'],
   build: {
     postcss: {
       postcssOptions: require('./postcss.config.js'),
