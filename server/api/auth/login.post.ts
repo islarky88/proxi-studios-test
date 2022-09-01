d3a2062252e355d841760861eaf2d9bbb35fbefc
Islarky Gwapito <islarky88@gmail.com>
2022-09-01 13:09:33 +0800

    install and prepare prisma and remote mysql database

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,10 +1,28 @@
+import bcrypt from 'bcryptjs';
+
 export default defineEventHandler(async (event) => {
   try {
-    const body = await useBody(event);
-    const env = useRuntimeConfig();
+    const secret = useRuntimeConfig().env?.public?.secret;
+    const { username, password } = await useBody(event);
 
-    console.log('login body :>> ', body);
-    console.log('env body :>> ', env);
+    if (!username || !password)
+      throw new Error('username or password is missing');
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    let token;
+
+    // try {
+    //    token = jwt.sign(
+    //      { userId: newUser.id, email: newUser.email },
+    //      'secretkeyappearshere',
+    //      { expiresIn: '1h' },
+    //    );
+    // } catch (error) {
+    //   console.log(error);
+    // }
+
     return {
       message: 'login success',
     };
